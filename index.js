ad7d2b2342ec94fc59fff848483796f54a9350c5
Daffa <daffaraihan03@gmail.com>
2022-07-06 09:53:22 +0700

    introduction

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.info('hello world')
+const nama = 'M. Daffa Raihan Akbar'
+const umur = 13
+const domisili = 'Bandung'
+
+function perkenalan() {
+    console.log(`Halo nama saya ${nama}, saya berumur ${umur} tahun, dan saya tinggal di ${domisili}`)
+}
+
+perkenalan();
